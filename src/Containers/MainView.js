2f9ab4ed934df3a55d79988faa1d201cad0e7729
jbae18 <jbae18@myseneca.ca>
2018-09-13 18:45:58 -0800

    ehllo world

--- src/Containers/MainView.js
+++ src/Containers/MainView.js
@@ -10,7 +10,7 @@ export default class MainView extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>MainView</Text>
+                <Text>Hello world</Text>
             </View>
         );
     }
